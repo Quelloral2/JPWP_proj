f199c4e16577ca9353d3b2ec09d3e8827e4b5a73
Quelloral2 <74183916+Quelloral2@users.noreply.github.com>
2020-11-09 15:50:14 +0100

    Initial commit

---
+++ desktop/src/com/jpwp/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.jpwp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jpwp.Gra;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.height = 720;
+		config.width = 1280;
+		config.resizable = false;
+		new LwjglApplication(new Gra(), config);
+	}
+}
